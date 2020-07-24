7b6fee06906c84643843c0b640ae94e34777b768
AlecM01 <61897324+AlecM01@users.noreply.github.com>
2020-07-24 15:29:10 -0500

    Add files via upload
    Didn't do much with it yet... Literally just a moveable player.

---
+++ rbCharecterController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TreeEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class rbCharecterController : MonoBehaviour
+{
+    #region gameObjects
+    public Rigidbody2D rb;
+    #endregion
+
+    #region settings
+    public float walkSpeed = 0f;
+    public float sprintSpeed = 0f;
+    #endregion
+
+    #region tools
+    private float speed;
+    #endregion
+
+    #region Input
+    public KeyCode forward = KeyCode.W;
+    public KeyCode left = KeyCode.A;
+    public KeyCode back = KeyCode.S;
+    public KeyCode right = KeyCode.D;
+    public KeyCode sprint = KeyCode.LeftShift;
+    #endregion
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+        speed = walkSpeed;
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        #region Input Manager
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        #endregion
+
+        #region Movement
+        float translation = v * speed * Time.deltaTime;
+        float straffe = h * speed * Time.deltaTime;
+        transform.Translate(straffe, translation, 0);
+        #endregion
+
+
+        #region Sprint
+        if (Input.GetKey(sprint))
+        {
+            speed = sprintSpeed;
+        }
+        else
+        {
+            speed = walkSpeed;
+        }
+        #endregion
+
+    }
+}
---
+++ spotLightController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Authentication.ExtendedProtection;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class spotLightController : MonoBehaviour
+{
+
+    #region settings
+    public float sensitivity = 2.0f;
+    public float smoothing = 1.0f;
+    #endregion
+
+    #region gameObjects
+    public GameObject player;
+    #endregion
+
+    #region vectors
+    public Vector2 playerMove;
+    public Vector2 smoothV;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = this.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var md = new Vector2(player.transform.position.x, player.transform.position.y);
+        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+        playerMove += smoothV;
+        transform.localRotation = Quaternion.AngleAxis(-playerMove.y, Vector2.right);
+        player.transform.localRotation = Quaternion.AngleAxis(playerMove.x, player.transform.up);
+
+    }
+}
